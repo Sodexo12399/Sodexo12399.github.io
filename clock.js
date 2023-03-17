ded6c7446b67cc996f76f957dfbf9cd6b263d611
Sodexo <126273036+Sodexo12399@users.noreply.github.com>
2023-03-17 21:52:37 +0100

    Add files via upload

--- clock.js
+++ clock.js
@@ -5,14 +5,6 @@ async function clock () {
   const hours = (((date.getHours() + 11) % 12) + 1)*30;
   const mn = (date.getMinutes())*6;
   const seconds = (date.getSeconds())*6;
-   
-
-  if((mn/6) >= 10){
-    span.innerHTML = `${hours / 30} : ${mn/6}`
-  }
-  else{
-    span.innerHTML = `${hours / 30} : 0${mn/6}`
-  }
 
     
   document.querySelector('.heure').style.transform = `rotate(${hours}deg)`
